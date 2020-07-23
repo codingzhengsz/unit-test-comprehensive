fe3aadaa3d086f530dd4f1c838322242feac8a84
Martin <codingzhengsz@163.com>
2020-07-23 21:20:17 +0800

    [GuessGame][Martin] refactor: refactor code to simplified code

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -6,32 +6,23 @@ import java.util.Set;
 public class GuessNumber {
 
     public String guess(int[] answer, int[] guessNumber) {
-        boolean isAllCorrect = true;
         Set<Integer> answerSet = new HashSet<>();
         Set<Integer> guessSet = new HashSet<>();
-        for (int index = 0; index < answer.length; index ++) {
-            if (answer[index] != guessNumber[index]) {
-                isAllCorrect = false;
-            }
-            answerSet.add(answer[index]);
-            guessSet.add(answer[index]);
+        for (int i : answer) {
+            answerSet.add(i);
+            guessSet.add(i);
         }
-        if (isAllCorrect) {
-            return "4A0B";
-        } else {
-            int count = 0;
-            int samePositionAndSameNumberCount = 0;
-            for (int index = 0; index < answer.length; index++) {
-                if (answer[index] == guessNumber[index]) {
-                    samePositionAndSameNumberCount ++;
-                } else {
-                    if (answerSet.contains(answer[index]) && guessSet.contains(guessNumber[index])) {
-                        count++;
-                    }
+        int notSamePositionAndSameNumberCount = 0;
+        int samePositionAndSameNumberCount = 0;
+        for (int index = 0; index < answer.length; index++) {
+            if (answer[index] == guessNumber[index]) {
+                samePositionAndSameNumberCount ++;
+            } else {
+                if (answerSet.contains(answer[index]) && guessSet.contains(guessNumber[index])) {
+                    notSamePositionAndSameNumberCount++;
                 }
             }
-            return String.format("%dA%dB",samePositionAndSameNumberCount, count);
         }
+        return String.format("%dA%dB",samePositionAndSameNumberCount, notSamePositionAndSameNumberCount);
     }
-
 }
