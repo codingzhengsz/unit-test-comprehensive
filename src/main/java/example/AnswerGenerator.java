9a400edc2923495b7bef332277ac5e9ce06205b1
Martin <codingzhengsz@163.com>
2020-07-23 19:59:40 +0800

    [GuessGame][Martin] refactor: refactor answer generator

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -1,9 +1,25 @@
 package example;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class AnswerGenerator {
 
     public int[] generateAnswer() {
-        return new int[]{10, 2, 3 ,1};
+        Random random = new Random();
+        int[] result = new int[4];
+        Set<Integer> counter = new HashSet<>();
+        int count = 0;
+        while (count < 4) {
+            int element = random.nextInt(10);
+            if (!counter.contains(element)){
+                counter.add(element);
+                result[count] = element;
+                count++;
+            }
+        }
+        return result;
     }
 
 }
