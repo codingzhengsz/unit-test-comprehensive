5dad364397694fcb2ba0452a6782b273a61d65bb
Martin <codingzhengsz@163.com>
2020-07-23 21:04:08 +0800

    [GuessGame][Martin] feature: pass should_return_0A2B_when_guess_given_answer_1234_and_input_guess_2156

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,19 +1,32 @@
 package example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GuessNumber {
 
     public String guess(int[] answer, int[] guessNumber) {
         boolean isAllCorrect = true;
+        Set<Integer> answerSet = new HashSet<>();
+        Set<Integer> guessSet = new HashSet<>();
         for (int index = 0; index < answer.length; index ++) {
             if (answer[index] != guessNumber[index]) {
                 isAllCorrect = false;
-                break;
             }
+            answerSet.add(answer[index]);
+            guessSet.add(answer[index]);
         }
         if (isAllCorrect) {
             return "4A0B";
+        } else {
+            int count = 0;
+            for (int index = 0; index < answer.length; index++) {
+                if (answerSet.contains(answer[index]) && guessSet.contains(guessNumber[index])) {
+                    count++;
+                }
+            }
+            return String.format("0A%dB", count);
         }
-        return "0A0B";
     }
 
 }
