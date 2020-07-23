27717494526538e89775690b4d9805cdcf8fd61a
Martin <codingzhengsz@163.com>
2020-07-23 21:33:01 +0800

    [GuessGame][Martin] feature: pass should_return_Wrong_Input_when_play_given_12

--- src/test/java/example/GameTest.java
+++ src/test/java/example/GameTest.java
@@ -24,4 +24,21 @@ public class GameTest {
         assertEquals("Wrong Input, Input again", result);
     }
 
+    @Test
+    public void should_return_Wrong_Input_when_play_given_12() {
+        // given
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        GuessNumber guessNumber = new GuessNumber();
+        ProcessController processController = new ProcessController();
+        Validator validator = new Validator();
+
+        Game game = new Game(answerGenerator, guessNumber, validator, processController);
+        int[] input = {1, 2};
+
+        // when
+        String result = game.play(input);
+
+        // then
+        assertEquals("Wrong Input, Input again", result);
+    }
 }
