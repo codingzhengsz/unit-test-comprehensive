24582b0382a85d9b4cd21ef5bfce3201216131af
Martin <codingzhengsz@163.com>
2020-07-23 20:48:17 +0800

    [GuessGame][Martin] feature: pass should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234

--- src/main/java/example/ResultGenerator.java
+++ src/main/java/example/ResultGenerator.java
@@ -3,6 +3,16 @@ package example;
 public class ResultGenerator {
 
     public String generateResult(int[] answer, int[] guessNumber) {
+        boolean isAllCorrect = true;
+        for (int index = 0; index < answer.length; index ++) {
+            if (answer[index] != guessNumber[index]) {
+                isAllCorrect = false;
+                break;
+            }
+        }
+        if (isAllCorrect) {
+            return "4A0B";
+        }
         return null;
     }
 
