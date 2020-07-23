7d00a631f6c2ef8959e64e29e49335c2e337c1f1
Martin <codingzhengsz@163.com>
2020-07-23 20:11:59 +0800

    [GuessGame][Martin] feature: pass should_return_true_when_validate_given_1234

--- src/main/java/example/Validator.java
+++ src/main/java/example/Validator.java
@@ -1,8 +1,19 @@
 package example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validator {
 
     public boolean validation(int[] guessAnswer) {
+        Set<Integer> counter = new HashSet<>();
+        for (int element : guessAnswer) {
+            if (counter.contains(element)) {
+                return false;
+            } else {
+                counter.add(element);
+            }
+        }
         return guessAnswer.length == 4;
     }
 
