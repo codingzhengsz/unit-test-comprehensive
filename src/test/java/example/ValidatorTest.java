716f92c37a7649a1a67ad14fc61d2ad39baa0873
Martin <codingzhengsz@163.com>
2020-07-23 20:17:31 +0800

    [GuessGame][Martin] test: should_return_false_when_validate_given_10_1_2_3

--- src/test/java/example/ValidatorTest.java
+++ src/test/java/example/ValidatorTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ValidatorTest {
 
     @Test
-    void should_return_true_when_validate_given_1234() {
+    void should_return_true_when_validate_given_1_2_3_4() {
         // given
         Validator validator = new Validator();
         int[] input = {1,2,3,4};
@@ -21,10 +21,23 @@ public class ValidatorTest {
     }
 
     @Test
-    void should_return_false_when_validate_given_2235() {
+    void should_return_false_when_validate_given_2_2_3_5() {
         // given
         Validator validator = new Validator();
         int[] input = {2,2,3,5};
+
+        // when
+        boolean result = validator.validation(input);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    void should_return_false_when_validate_given_10_1_2_3() {
+        // given
+        Validator validator = new Validator();
+        int[] input = {10, 1, 2, 3};
 
         // when
         boolean result = validator.validation(input);
