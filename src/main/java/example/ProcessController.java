c0c7b5f51476ef42db64347c083cbe05fe8ad945
Martin <codingzhengsz@163.com>
2020-07-27 21:19:27 +0800

    [GuessGame][Martin] refactor: refactor code to change process

--- src/main/java/example/ProcessController.java
+++ src/main/java/example/ProcessController.java
@@ -1,15 +1,37 @@
 package example;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class ProcessController {
 
-    public String processControl(int frequency, String result) {
-        if (frequency > 6) {
-            return "Fail";
-        }
-        if ("4A0B".equals(result)) {
-            return "Success";
-        }
-        return result;
-    }
+  private Validator validator;
+  private GuessNumber guessNumber;
 
+  public ProcessController(Validator validator, GuessNumber guessNumber) {
+    this.validator = validator;
+    this.guessNumber = guessNumber;
+  }
+
+  public String processControl(int[] answer) {
+    Scanner scanner = new Scanner(System.in);
+
+    for (int i = 0; i < 6; i++) {
+      String guessAnswer = scanner.next();
+      int[] guessAnswerArray =
+          Stream.of(guessAnswer.split("")).mapToInt(Integer::parseInt).toArray();
+      boolean isPass = validator.validation(guessAnswerArray);
+      if (isPass) {
+        String result = guessNumber.guess(answer, guessAnswerArray);
+        if ("4A0B".equals(result)) {
+          return "4A0B";
+        } else {
+          System.out.print(result + "\n");
+        }
+      } else {
+        System.out.print("Wrong Input,Input again\n");
+      }
+    }
+    return "You are Fail";
+  }
 }
--- src/test/java/example/ProcessControllerTest.java
+++ src/test/java/example/ProcessControllerTest.java
@@ -2,50 +2,72 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProcessControllerTest {
 
-    @Test
-    void should_return_Success_when_process_given_5_and_4A0B() {
-        // given
-        ProcessController controller = new ProcessController();
-        int frequency = 5;
-        String result = "4A0B";
+  @Test
+  void should_return_4A0B_when_process_three_times_and_given_correct_answer() {
+    // given
+    Validator validator = new Validator();
+    GuessNumber guessNumber = new GuessNumber();
+    ProcessController controller = new ProcessController(validator, guessNumber);
+    String input = "1324\n1521\n1234";
+    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
 
-        // when
-        String isContinue = controller.processControl(frequency, result);
+    int[] answer = new int[] {1, 2, 3, 4};
+    // when
+    String result = "";
+    result = controller.processControl(answer);
 
-        // then
-        assertEquals("Success", isContinue);
-    }
+    // then
+    assertEquals("4A0B", result);
+  }
 
-    @Test
-    void should_return_Fail_when_process_control_given_7_and_4A0B() {
-        // given
-        ProcessController controller = new ProcessController();
-        int frequency = 7;
-        String result = "4A0B";
+  @Test
+  void should_return_2A0B_when_process_six_times_and_given_guess_number_1258_and_answer_1234() {
+    // given
+    Validator validator = new Validator();
+    GuessNumber guessNumber = new GuessNumber();
+    ProcessController controller = new ProcessController(validator, guessNumber);
+    String input = "1258\n1258\n1258\n1258\n1258\n1258";
+    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setIn(in);
+    System.setOut(new PrintStream(outContent));
 
-        // when
-        String isContinue = controller.processControl(frequency, result);
+    int[] answer = new int[] {1, 2, 3, 4};
+    // when
+    String result = controller.processControl(answer);
 
-        // then
-        assertEquals("Fail", isContinue);
-    }
+    // then
+    assertEquals("2A0B\n2A0B\n2A0B\n2A0B\n2A0B\n2A0B\n", outContent.toString());
+    assertEquals("You are Fail", result);
+  }
 
-    @Test
-    void should_return_2A2B_when_process_control_given_3_and_2A2B() {
-        // given
-        ProcessController controller = new ProcessController();
-        int frequency = 3;
-        String result = "2A2B";
+  @Test
+  void
+      should_return_WrongInput_when_process_six_times_and_given_guess_number_1258_five_times_and_12_once_and_answer_1234() {
+    // given
+    Validator validator = new Validator();
+    GuessNumber guessNumber = new GuessNumber();
+    ProcessController controller = new ProcessController(validator, guessNumber);
+    String input = "1258\n1258\n1258\n1258\n1258\n12";
+    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setIn(in);
+    System.setOut(new PrintStream(outContent));
 
-        // when
-        String isContinue = controller.processControl(frequency, result);
+    int[] answer = new int[] {1, 2, 3, 4};
+    // when
+    controller.processControl(answer);
 
-        // then
-        assertEquals("2A2B", isContinue);
-    }
-
+    // then
+    assertEquals("2A0B\n2A0B\n2A0B\n2A0B\n2A0B\nWrong Input,Input again\n", outContent.toString());
+  }
 }
