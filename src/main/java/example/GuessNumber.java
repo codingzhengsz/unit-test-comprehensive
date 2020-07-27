daf5ba9d5e9c5fb1ea0fc004dd303da2894ca0ed
Martin <codingzhengsz@163.com>
2020-07-27 20:32:29 +0800

    [GuessGame][Martin] refactor: extract a format method in GuessNumber

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -5,24 +5,32 @@ import java.util.Set;
 
 public class GuessNumber {
 
-    public String guess(int[] answer, int[] guessNumber) {
-        Set<Integer> answerSet = new HashSet<>();
-        Set<Integer> guessSet = new HashSet<>();
-        for (int i : answer) {
-            answerSet.add(i);
-            guessSet.add(i);
-        }
-        int notSamePositionAndSameNumberCount = 0;
-        int samePositionAndSameNumberCount = 0;
-        for (int index = 0; index < answer.length; index++) {
-            if (answer[index] == guessNumber[index]) {
-                samePositionAndSameNumberCount ++;
-            } else {
-                if (answerSet.contains(answer[index]) && guessSet.contains(guessNumber[index])) {
-                    notSamePositionAndSameNumberCount++;
-                }
-            }
-        }
-        return String.format("%dA%dB",samePositionAndSameNumberCount, notSamePositionAndSameNumberCount);
+  public String guess(int[] answer, int[] guessNumber) {
+    Set<Integer> answerSet = new HashSet<>();
+    Set<Integer> guessSet = new HashSet<>();
+    for (int i : answer) {
+      answerSet.add(i);
     }
+    for (int i : guessNumber) {
+      guessSet.add(i);
+    }
+    int notSamePositionAndSameNumberCount = 0;
+    int samePositionAndSameNumberCount = 0;
+    for (int index = 0; index < answer.length; index++) {
+      if (answer[index] == guessNumber[index]) {
+        samePositionAndSameNumberCount++;
+      } else {
+        if (guessSet.contains(answer[index])) {
+          notSamePositionAndSameNumberCount++;
+        }
+      }
+    }
+    return formatResultMsg(notSamePositionAndSameNumberCount, samePositionAndSameNumberCount);
+  }
+
+  private String formatResultMsg(
+      int notSamePositionAndSameNumberCount, int samePositionAndSameNumberCount) {
+    return String.format(
+        "%dA%dB", samePositionAndSameNumberCount, notSamePositionAndSameNumberCount);
+  }
 }
